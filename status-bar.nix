f4596fed5805bb72532461bf415074d138317764
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-02-20 00:38:45 +0100

    Update rust deps

--- status-bar.nix
+++ status-bar.nix
@@ -3,6 +3,6 @@ rustPlatform.buildRustPackage rec {
   pname = "status-bar";
   version = "0.1.0";
   src = ./.;
-  cargoSha256 = "WuogSTbgkAvKvmUgNiQK2RE18f8fya2uFpVqF3eIaWA=";
+  cargoSha256 = "QkKrGWN1wx5DW+vV5ngGBD91IReqg0adMydDeOfYD6A=";
   meta = with lib; { description = "A minimal status bar for swaywm"; };
 }
